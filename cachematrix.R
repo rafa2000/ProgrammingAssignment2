0518e7617bcb66ae112eaf126dbe26eb660c803e
Rafael A. Salazar <rafael.a.salazar@gmail.com>
2016-06-05 16:51:36 -0400

    Add comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## `makeCacheMatrix` creates a special matrix that can hold its cached inverse
+##
+## internal fields
+## x : cached matrix
+## i : cached inverse
+##
+## To explore:
+## use matrix equality to avoid assignment when matrix is already cached
+## https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html?BHT-9c73f427-7e3a-bc46-bc8a-5ff5849f6d30.0
 
 makeCacheMatrix <- function(x = matrix()) {
-  # internal fields
-  # x : cached matrix
-  # i : cached inverse
-  
-  # explore:
-  # use matrix equality to avoid assignment when matrix is already cached
-  # https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html?BHT-9c73f427-7e3a-bc46-bc8a-5ff5849f6d30.0
   i <- NULL
   set <- function(y) {
     x <<- y
@@ -26,12 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
   )
 }
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## If there is a cached inverse return that,
+## if not calculate, cache, and return the inverse of the matrix
+##
+## `x`: is the special matrix made by makeCacheMatrix
+##  ex: makeCacheMatrix(matrix(c(3,1,7,5)),2,2)
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  ## `x`: is the special matrix made by makeCacheMatrix
-  ##  ex: makeCacheMatrix(matrix(c(3,1,7,5)),2,2)
   i <- x$getInverse()
   if(!is.null(i)) {
     message("getting cached inverse")
